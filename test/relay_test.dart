f3ec615a2d4ade76832b1c17c23119ab93fc66a4
Ulrich Raab <ulraab@gmail.com>
2022-11-21 17:46:53 +0100

    Update relay test

--- test/relay_test.dart
+++ test/relay_test.dart
@@ -2,41 +2,23 @@ import 'package:nostr_client/nostr_client.dart';
 import 'package:test/test.dart';
 
 void main() {
-  late final Relay relay;
-
-  setUp(() {
-    relay = Relay.connect('wss://relay.nostr.info');
-    relay.stream.listen(printMessage);
-  });
+  final relayUrl = 'wss://relay.nostr.info';
 
   test(
-    'req',
+    'req kind 0 events (user metadata)',
     () async {
-      final filter = Filter(
-        kinds: [1],
-        limit: 100,
-      );
+      final relay = Relay.connect(relayUrl);
+      relay.stream.whereIsEvent().whereIsKind(0).listen((event) {
+        final jsonString = event.content;
+        final userMetadata = UserMetadata.fromJsonString(jsonString);
+        final p = userMetadata.toString();
+        print(p);
+      });
 
-      for (int i = 0; i < 1; i++) {
-        final subscriptionId = relay.req(filter);
-        await Future.delayed(Duration(seconds: 5));
-        return relay.close(subscriptionId);
-      }
+      final filter = Filter(kinds: [0]);
+      final subscriptionId = relay.req(filter);
+      await Future.delayed(Duration(seconds: 5));
+      return relay.close(subscriptionId);
     },
   );
-}
-
-printMessage(List<dynamic> message) {
-  final type = message.first;
-  if (type == 'EVENT') {
-    final json = message[2];
-    printEvent(json);
-  } else {
-    print('Message(type: $type)');
-  }
-}
-
-printEvent(Map<String, dynamic> json) {
-  final event = Event.fromJson(json);
-  print(event.toString());
 }
