9d001fcef275f882516e67631de770a7a3bd3852
Ulrich Raab <ulraab@gmail.com>
2022-11-21 20:18:37 +0100

    Move relay and information document to relay dir

--- lib/nostr_client.dart
+++ lib/nostr_client.dart
@@ -7,8 +7,8 @@ export 'package:nostr_client/src/key_pair/generator/mnemonic_key_pair_generator.
 export 'package:nostr_client/src/key_pair/generator/random_key_pair_generator.dart';
 export 'package:nostr_client/src/key_pair/key_pair.dart';
 export 'package:nostr_client/src/message.dart';
-export 'package:nostr_client/src/relay.dart';
-export 'package:nostr_client/src/relay_information_document.dart';
+export 'package:nostr_client/src/relay/relay.dart';
+export 'package:nostr_client/src/relay/relay_information_document.dart';
 export 'package:nostr_client/src/signature/signer.dart';
 export 'package:nostr_client/src/signature/verifier.dart';
 export 'package:nostr_client/src/user_metadata.dart';
