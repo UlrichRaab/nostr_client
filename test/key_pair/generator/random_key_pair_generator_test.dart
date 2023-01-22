546d7c111e541a4301becd654feae54a7bee724d
Ulrich Raab <ulraab@gmail.com>
2023-01-22 17:13:48 +0100

    Update test

--- test/key_pair/generator/random_key_pair_generator_test.dart
+++ test/key_pair/generator/random_key_pair_generator_test.dart
@@ -4,12 +4,12 @@ import 'package:test/test.dart';
 void main() {
   final generator = RandomKeyPairGenerator();
 
-  test(
-    'generate',
-    () async {
-      for (int i = 0; i < 10; i++) {
-        final keyPair = generator.generate();
-      }
-    },
-  );
+  test('generate', () async {
+    final keyPairs = <KeyPair>{};
+    for (int i = 0; i < 10; i++) {
+      final keyPair = generator.generate();
+      keyPairs.add(keyPair);
+    }
+    assert(keyPairs.length == 10);
+  });
 }
