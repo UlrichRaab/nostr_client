83a573971b567adbd60d9a0720da087fdc531aef
Ulrich Raab <ulraab@gmail.com>
2023-01-22 17:09:44 +0100

    Update example

--- example/nostr_client_example.dart
+++ example/nostr_client_example.dart
@@ -1,1 +1,23 @@
-void main() {}
+import 'package:nostr_client/nostr_client.dart';
+
+void main() {
+  // Create a new relay instance and connect to the relay
+  final relay = Relay('wss://relay.nostr.info');
+  relay.connect();
+
+  // Print events sent by the relay
+  relay.stream.whereIsEvent().listen(print);
+
+  // Request text events from the relay and subscribe to updates
+  final filter = Filter(
+    kinds: [EventKind.text],
+    limit: 10,
+  );
+  final subscriptionId = relay.subscribe(filter);
+
+  // Cancel the subscription
+  relay.unsubscribe(subscriptionId);
+
+  // Disconnect from the relay
+  relay.disconnect();
+}
